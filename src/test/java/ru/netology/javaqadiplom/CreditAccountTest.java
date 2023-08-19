001fc6c93c91acca931961e84ce4b8bb15f24e57
Виктория <viksyaavito@gmail.com>
2023-08-19 14:33:38 +0300

    CreditAccount tests: constructor and pay method

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -5,16 +5,37 @@ import org.junit.jupiter.api.Test;
 
 public class CreditAccountTest {
 
-    @Test
-    public void shouldAddToPositiveBalance() {
-        CreditAccount account = new CreditAccount(
-                0,
-                5_000,
-                15
-        );
-
-        account.add(3_000);
-
-        Assertions.assertEquals(3_000, account.getBalance());
+    @Test void testIncorrectRate() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new CreditAccount(1000, 2000, -10);
+        });
+    }
+    @Test void testIncorrectInitialBalance() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new CreditAccount(-1000, 2000, 10);
+        });
+    }
+    @Test void testIncorrectInitialCreditLimit() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new CreditAccount(1000, -2000, 10);
+        });
+    }
+    @Test void testPositiveAmountPay() {
+        CreditAccount account = new CreditAccount(1000, 2000, 10);
+        boolean payResult = account.pay(500);
+        Assertions.assertTrue(payResult);
+        Assertions.assertEquals(500, account.getBalance());
+    }
+    @Test void testNegativeAmountPay() {
+        CreditAccount account = new CreditAccount(1000, 2000, 10);
+        boolean payResult = account.pay(-500);
+        Assertions.assertFalse(payResult);
+        Assertions.assertEquals(1000, account.getBalance());
+    }
+    @Test void testOutOfLimitPay() {
+        CreditAccount account = new CreditAccount(1000, 2000, 10);
+        boolean payResult1 = account.pay(3500);
+        Assertions.assertFalse(payResult1);
+        Assertions.assertEquals(1000, account.getBalance());
     }
 }
