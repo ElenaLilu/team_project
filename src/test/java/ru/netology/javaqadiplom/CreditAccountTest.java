67ba63286a696259a8c320cfbf739af49938f528
Виктория <viksyaavito@gmail.com>
2023-08-19 14:48:14 +0300

    CreditAccount tests: constructor and pay method

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class CreditAccountTest {
 
+    // Constructor
     @Test void testIncorrectRate() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             new CreditAccount(1000, 2000, -10);
@@ -20,6 +21,8 @@ public class CreditAccountTest {
             new CreditAccount(1000, -2000, 10);
         });
     }
+
+    // Pay
     @Test void testPositiveAmountPay() {
         CreditAccount account = new CreditAccount(1000, 2000, 10);
         boolean payResult = account.pay(500);
@@ -36,6 +39,26 @@ public class CreditAccountTest {
         CreditAccount account = new CreditAccount(1000, 2000, 10);
         boolean payResult1 = account.pay(3500);
         Assertions.assertFalse(payResult1);
+        Assertions.assertEquals(1000, account.getBalance());
+    }
+
+    // Add
+    @Test void testPositiveAmountAdd() {
+        CreditAccount account = new CreditAccount(1000, 2000, 10);
+        boolean addResult = account.add(10000);
+        Assertions.assertTrue(addResult);
+        Assertions.assertEquals(11000, account.getBalance());
+    }
+    @Test void testNegativeAmountAdd() {
+        CreditAccount account = new CreditAccount(1000, 2000, 10);
+        boolean addResult = account.add(-10);
+        Assertions.assertFalse(addResult);
+        Assertions.assertEquals(1000, account.getBalance());
+    }
+    @Test void testEmptyAmoundAdd() {
+        CreditAccount account = new CreditAccount(1000, 2000, 10);
+        boolean addResult = account.add(0);
+        Assertions.assertFalse(addResult);
         Assertions.assertEquals(1000, account.getBalance());
     }
 }
