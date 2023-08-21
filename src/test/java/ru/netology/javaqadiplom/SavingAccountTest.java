aae302478e2c3d0541fcb8cfbb8ad329591d7880
ElenaLilu <elena.imyarekova1@gmail.com>
2023-08-21 15:46:04 +0300

    src

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -18,4 +18,208 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+
+    @Test
+    public void shouldAddEqualsMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                 2_000,
+                 1_000,
+                10_000,
+                5
+        );
+        account.add(10_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddMoreThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(11_000);
+
+        Assertions.assertFalse(false);
+    }
+
+    @Test
+    public void shouldAddZero() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(0);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddLessThanInitialBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(1_500);
+
+        Assertions.assertEquals(1_500, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddNegative() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(-2);
+
+        Assertions.assertFalse(false);
+    }
+
+    @Test
+    public void shouldPayThanLessMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(5_000);
+
+        Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayThanLessInitialBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(1_000);
+
+        Assertions.assertEquals(1_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayThanMoreInitialBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(4_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayNegative() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(-1_000);
+
+        Assertions.assertFalse(false);
+    }
+
+    @Test
+    public void shouldPayZero() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(0);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayThanMoreMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(12_000);
+
+        Assertions.assertFalse(false);
+    }
+
+    @Test
+    public void shouldPayThanMoreMinBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(1_500);
+
+        Assertions.assertFalse(false);
+    }
+
+
+    @Test
+    public void shouldYearChange() {
+
+        SavingAccount account = new SavingAccount(
+                30_000,
+                20_000,
+                100_000,
+                15
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(4_500, account.yearChange());
+    }
+
+    @Test
+    public void TestYearChangeZero() {
+
+        SavingAccount account = new SavingAccount(
+                0,
+                0,
+                10_000,
+                5
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void TestYearChangeEqualsRate() {
+
+        SavingAccount account = new SavingAccount(
+                0,
+                0,
+                10_000,
+                5
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+
 }
